8057972f1857c2d5a9f83f80f6eb9fbc9ce5a507
Bear <xiongran@zmit.cn>
2015-02-06 10:18:00 +0800

    Initial Commit

---
+++ CircleAnimationTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CircleAnimationTest
+//
+//  Created by zmit on 15/2/6.
+//  Copyright (c) 2015年 zmit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
